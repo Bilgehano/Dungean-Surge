cefa2420f13f17b78c693b5e2ebb88e9c1169495
Bilgehano <blghnoznvr@gmail.com>
2026-04-29 11:24:33 +0200

    Level1 first design edited tilemaps and created buildings

---
+++ Assets/Scripts/Elevation_Entry.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Elevation_Entry : MonoBehaviour
+{
+    public Collider2D[] mountainCollider;
+    public Collider2D[] boundaryCollider;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            foreach (Collider2D mountain in mountainCollider)
+            {
+            mountain.enabled = false;
+            }
+            foreach (Collider2D boundary in boundaryCollider)
+            {
+                boundary.enabled = true;
+            }
+        collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+
+
+
+        }
+
+      
+
+    }
+
+}
---
+++ Assets/Scripts/Elevation_Exit.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Elevation_Exit : MonoBehaviour
+{
+    public Collider2D[] mountainCollider;
+    public Collider2D[] boundaryCollider;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            foreach (Collider2D mountain in mountainCollider)
+            {
+            mountain.enabled = true;
+            }
+            foreach (Collider2D boundary in boundaryCollider)
+            {
+                boundary.enabled = false;
+            }
+        collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 10;
+
+
+
+        }
+
+      
+
+    }
+
+}
