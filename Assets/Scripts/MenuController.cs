bc39373aeb67c18bbb0f089687ee4fddf512cfa3
Teirih <selimberk.tan@studmail.htw-aalen.de>
2026-05-13 19:24:40 +0200

    Add new UI assets and metadata for Tiny Swords project
    - Added WoodTable_Slots.png and its metadata for UI elements.
    - Created UIs directory and added multiple UI images including Gemini_Generated_Image, Main Page UI, MainBackground UI, MainPage UI, and Options Page UI, along with their respective metadata files.
    - Included a video asset (ed_c_e_e_e_cf_e_amp_.mp4) and its metadata for UI purposes.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using JetBrains.Annotations;
+
+public class MenuController : MonoBehaviour
+{   
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [SerializeField] private float defaultVolume = 0.5f;
+
+    [Header("Levels to Load")]
+    public string _newgameLevel;
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newgameLevel);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();    
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    } 
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+       confirmationPrompt.SetActive(true);
+       yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+}
